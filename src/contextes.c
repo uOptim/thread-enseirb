aa822d18dd6cbb9833a2b9bb11fc3d2c260fa8e4
uOptim <uOptim@fakeemail.com>
2013-04-02 15:05:16 +0200

    exemple 3 contextes sans passer par main

--- src/contextes.c
+++ src/contextes.c
@@ -8,27 +8,31 @@ void func(int numero)
 }
 
 int main() {
-  ucontext_t uc, previous;
+  ucontext_t uc1, uc2, uc3, previous;
 
-  getcontext(&uc); /* initialisation de uc avec valeurs coherentes
-		    * (pour éviter de tout remplit a la main ci-dessous) */
+  getcontext(&uc1);
+  getcontext(&uc2);
+  getcontext(&uc3);
 
-  uc.uc_stack.ss_size = 64*1024;
-  uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
-  uc.uc_link = &previous;
-  makecontext(&uc, (void (*)(void)) func, 1, 34);
+  uc1.uc_stack.ss_size = 64*1024;
+  uc1.uc_stack.ss_sp = malloc(uc1.uc_stack.ss_size);
+  uc1.uc_link = NULL;
+  makecontext(&uc1, (void (*)(void)) func, 1, 34);
+
+  uc2.uc_stack.ss_size = 64*1024;
+  uc2.uc_stack.ss_sp = malloc(uc2.uc_stack.ss_size);
+  uc2.uc_link = &uc1;
+  makecontext(&uc2, (void (*)(void)) func, 1, 57);
+
+  uc3.uc_stack.ss_size = 64*1024;
+  uc3.uc_stack.ss_sp = malloc(uc3.uc_stack.ss_size);
+  uc3.uc_link = &uc2;
+  makecontext(&uc3, (void (*)(void)) func, 1, 67);
 
   printf("je suis dans le main\n");
-  swapcontext(&previous, &uc);
+  swapcontext(&previous, &uc3);
   printf("je suis revenu dans le main\n");
 
-  uc.uc_stack.ss_size = 64*1024;
-  uc.uc_stack.ss_sp = malloc(uc.uc_stack.ss_size);
-  uc.uc_link = NULL;
-  makecontext(&uc, (void (*)(void)) func, 1, 57);
-
-  printf("je suis dans le main\n");
-  setcontext(&uc);
-  printf("je ne reviens jamais ici\n");
+  printf("Quit depuis le main\n");
   return 0;
 }

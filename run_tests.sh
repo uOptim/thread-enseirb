09c2d1189786580aa85031b1dffcbd654b779d89
uOptim <uOptim@fakeemail.com>
2013-05-15 08:54:40 +0200

    script pour lancer les tests

---
+++ run_tests.sh
@@ -0,0 +1,38 @@
+echo "TEST: 01-main"
+./tests/01-main 
+echo "------------------------------------------------"
+echo "TEST: 02-switch"
+./tests/02-switch
+echo "------------------------------------------------"
+echo "TEST: 11-join"
+./tests/11-join
+echo "------------------------------------------------"
+echo "TEST: 12-join-main"
+./tests/12-join-main
+echo "------------------------------------------------"
+echo "TEST: 13-join-cascade 200"
+./tests/13-join-cascade 200
+echo "------------------------------------------------"
+echo "TEST: 21-create-many 200"
+./tests/21-create-many 200
+echo "------------------------------------------------"
+echo "TEST: 22-create-many-recursive 200"
+./tests/22-create-many-recursive 200
+echo "------------------------------------------------"
+echo "TEST: 31-switch-many 100 200"
+./tests/31-switch-many 100 200
+echo "------------------------------------------------"
+echo "TEST: 32-switch-many-join 100 200"
+./tests/32-switch-many-join 100 200
+echo "------------------------------------------------"
+echo "TEST: 51-fibonacci 23"
+./tests/51-fibonacci 23
+echo "------------------------------------------------"
+echo "TEST: 52-array-sum 1000"
+./tests/52-array-sum 1000
+echo "------------------------------------------------"
+echo "TEST: 53-quicksort 100"
+./tests/53-quicksort 100
+echo "------------------------------------------------"
+echo "TEST: 54-mergesort 100"
+./tests/54-mergesort 100
